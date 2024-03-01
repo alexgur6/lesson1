8da153e54ed8b94801f36a31abc4640f6454a363
alexgur6 <161103571+alexgur6@users.noreply.github.com>
2024-03-02 02:07:09 +0400

    Create z23.py

---
+++ z23.py
@@ -0,0 +1,7 @@
+def my_filter(a):
+    result = ' '.join(map(str, [int(x) * 10 for x in a]))
+    return result
+
+input_list = input("Введите числа через пробел: ").split()
+output_string = my_filter(input_list)
+print(output_string)
