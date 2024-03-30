255ff7213b2974b70c1ff1d0da1aa8367c1a356a
alexgur6 <161103571+alexgur6@users.noreply.github.com>
2024-03-30 08:22:00 +0400

    Update zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -1,19 +1,2 @@
 x=36/6*2
 print(x)
-import tkinter as tk 
-import webbrowser 
- 
-def open_random_article(): 
- webbrowser.open("https://en.wikipedia.org/wiki/Special:Random") 
- 
-app = tk.Tk() 
-app.title("Случайная страница") 
-app.geometry("300x100") 
- 
-label = tk.Label(app, text="Хотите увидеть случайную страницу Википедии?") 
-label.pack() 
- 
-open_button = tk.Button(app, text="Open Random Article", command=open_random_article) 
-open_button.pack() 
- 
-app.mainloop()
